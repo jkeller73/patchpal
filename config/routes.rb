bd95863aefe7ea32f8b7d520ad0ae07693ad1b15
Juliette Keller <kellerjuliette@hotmail.com>
2019-11-25 17:45:10 +0000

    basic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
   resources :plants, only: [:index, :show]
   resources :patches do
     resources :patch_plants, only: [:create, :destroy]
   end
   devise_for :users
-  root to: "home#index"
 end
