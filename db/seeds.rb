f8cc300ac651797f8e549d16ec02cd3682e902eb
Alice Gregson <gregsonalice1@gmail.com>
2019-11-26 15:35:47 +0000

    showpage CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  # puts "Cleaning database..."
+  # Plant.destroy_all
+
+  # puts "Creating plants..."
+  # plant1 = Plant.create(name: "Potato", description: "potato des.", hardiness: "hard", position_in: "sun", sowing: "summer", more_info: "more info ")
+  # plant2 = Plant.create(name: "Leek", description: "leed des.", hardiness: "not hard", position_in: "not sun", sowing: "summer", more_info: "more info 2")
+
+  # puts "Finished!"
+
