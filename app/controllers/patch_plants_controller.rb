0d0917a8bdbfa2e6cff7329596fdd26087b4f037
Jana Gawthorpe <jana.melisko@gmail.com>
2019-11-26 12:24:15 +0000

    updated controllers

--- app/controllers/patch_plants_controller.rb
+++ app/controllers/patch_plants_controller.rb
@@ -1,7 +1,30 @@
 class PatchPlantsController < ApplicationController
   def create
+    @patch = Patch.find(params[:patch_id])
+    @patch_plant = PatchPlant.new(patch_plant_params)
+    @patch_plant.patch = @patch
+    if @patch_plant.save
+      redirect_to patch_path(@patch)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @patch_plant = PatchPlant.find(params[:id])
+    @patch_plant.update(patch_plant_params)
+    redirect_to patch_path(@patch)
   end
 
   def destroy
+    @patch_plant = PatchPlant.find(params[:id])
+    @patch_plant.destroy
+    redirect_to patch_path(@patch_plant.patch)
+  end
+
+  private
+
+  def patch_plant_params
+    params.require(:patch_path).permit(:plant_date, :plant)
   end
 end
--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -1,22 +1,49 @@
 class PatchesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_patch, only: [:show, :edit, :update, :destroy]
+
   def index
+    @patches = Patch.all
   end
 
   def show
   end
 
   def new
+    @patch = Patch.new
   end
 
   def create
+    @patch = Patch.new(patch_params)
+    @patch.user = current_user
+
+    if @patch.save
+      redirect_to patch_path(@patch)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @patch.update(patch_params)
+    redirect_to patch_path(@patch)
   end
 
   def destroy
+    @patch.destroy
+    redirect_to patches_path
+  end
+
+  private
+
+  def find_patch
+    @patch = Patch.find(params[:id])
+  end
+
+  def  patch_params
+    params.require(:patch).permit(:address)
   end
 end
