7d0644c0993767caac0e6d33a212b56de794eadc
Alice Gregson <gregsonalice1@gmail.com>
2019-11-27 12:17:24 +0000

    updated devise for signing up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @home = true
   end
 
   def dashboard
-    @patches = curren_user.patches
+    @patches = current_user.patches
   end
 end
---
+++ app/policies/patch_policy.rb
@@ -0,0 +1,7 @@
+class PatchPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
