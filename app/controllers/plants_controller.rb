1d4d7c185a8fd09c46361f1f7a5bec2a7d076a81
Alice Gregson <gregsonalice1@gmail.com>
2019-11-27 13:38:04 +0000

    added pundit for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,12 @@
 class PlantsController < ApplicationController
   def index
-    @plants = Plant.all
+    # @plants = Plant.all
+    @plants = policy_scope(Plant)
   end
 
   def show
     @plant = Plant.find(params[:id])
+    authorize @plant
+
   end
 end
