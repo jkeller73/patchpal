50c1292d866af535d70946016e9d31f37a9c85d2
Juliette Keller <kellerjuliette@hotmail.com>
2019-11-28 11:43:08 +0000

    fixed the weather api

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Patch < ApplicationRecord
   belongs_to :user
   has_many :patch_plants, dependent: :destroy
@@ -8,4 +10,12 @@ class Patch < ApplicationRecord
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def update_weather
+    forecast = JSON.parse(open("http://api.openweathermap.org/data/2.5/forecast?lat=#{longitude}&lon=#{latitude}&APPID=#{ENV['WEATHER_API_KEY']}").read)
+    days_array = forecast['list']
+    @description = days_array[0]['weather'][0]['description']
+    @temperature = (days_array[0]['main']['temp'] - 273.15).round
+    DailyWeatherReport.create(date: Time.now, description: @description, temperature: @temperature, patch: self)
+  end
 end
