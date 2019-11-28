63667df30e228de3de6b9e5f1ff66c86e326d273
Olly Taylor <ollyjtaylor@live.co.uk>
2019-11-28 11:53:19 +0000

    css fixes

--- app/policies/patch_policy.rb
+++ app/policies/patch_policy.rb
@@ -17,6 +17,10 @@ class PatchPolicy < ApplicationPolicy
     true
   end
 
+  def new?
+    true
+  end
+
   def update?
     user_is_owner?
     # record.user == user
@@ -32,5 +36,4 @@ class PatchPolicy < ApplicationPolicy
   def user_is_owner?
     record.user == user
   end
-
 end
