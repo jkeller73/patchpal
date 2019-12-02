f4cb6418123f9abcecacf786ee3abc3547223d33
Juliette Keller <kellerjuliette@hotmail.com>
2019-12-02 12:30:57 +0000

    fixed the login page and did the seasonal suggestion

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ vegetable_suffixes = ['tomato-supersweet/txz61714TM', 'carrot-mokum-f1-hybrid/76
 'kale-midnight-sun/tm60213TM', 'swiss-chard-fantasy/gww4969TM', 'beetroot-kestrel-f1-hybrid-globe/534TM',
 'sweet-pepper-gourmet/62TM', 'chilli-pepper-hot-thai/tm75329TM','pea-ceresa-petit-pois/gww4786TM',
 'broad-bean-jubilee-hysor/802TM','parsley-laura/862TM', 'basil-christmas/4886TM', 'coriander/481TM',
-'lettuce-sweet-success-icebergromaine/TT50586TM', 'radish-mars-f1-hybrid/tr54728TM', 'cucumber-diva/TT52081TM']
+'lettuce-sweet-success-icebergromaine/TT50586TM', 'radish-mars-f1-hybrid/tr54728TM', 'cucumber-diva/TT52081TM', 'lettuce-winter-gem-cos/999TM', 'broad-bean-aquadulce-claudia/138TM']
 
 array = []
 vegetable_suffixes.each do |suffix|
