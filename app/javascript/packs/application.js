9710e20b83858e6d2c405284450d022c7d3db9ba
Alice Gregson <gregsonalice1@gmail.com>
2019-12-04 16:24:36 +0000

    added Sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import places from 'places.js';
+import { initSweetalert } from '../plugins/init_sweetalert';
+import { initSweetalertWater } from '../plugins/init_sweetalert';
 
 if (document.querySelector('#patch_address')) {
   const placesAutocomplete = places({
@@ -10,16 +12,21 @@ if (document.querySelector('#patch_address')) {
 }
 
 
-// import { initSweetalert } from '../plugins/init_sweetalert';
+initSweetalert('#sweet-alert-demo', {
+  title: "Nice One!",
+  text: "You've sowed your seeds, we'll remind you when to water them and when they're ready to harvest!",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
 
-// initSweetalert('#sweet-alert-demo', {
-//   title: "A nice alert",
-//   text: "This is a great alert, isn't it?",
-//   icon: "success"
-// }, (value) => {
-//   console.log(value);
-// });
-
+initSweetalertWater('#sweet-alert-demo', {
+  title: "Nice One!",
+  text: "Well done - you have watered your plants. We'll track the weather for you and remind you if you need to water them again!",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
 
 
 
--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,16 +1,25 @@
-// import swal from 'sweetalert';
+import swal from 'sweetalert';
 
-// const initSweetalert = (selector, options = {}, callback = () => {}) => {
-//   const swalButton = document.getElementById('patch-plus-button');
-//   if (swalButton) { // protect other pages
-//     swalButton.addEventListener('click', () => {
-//       swal(options).then(callback); // <-- add the `.then(callback)`
-//     });
-//   }
-// };
+const initSweetalert = (selector, options = {}, callback = () => {}) => {
+  const swalButton = document.getElementById('sow-button');
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options).then(callback); // <-- add the `.then(callback)`
+    });
+  }
+};
 
-// export { initSweetalert };
-
-
+export { initSweetalert };
 
 
+
+const initSweetalertWater = (selector, options = {}, callback = () => {}) => {
+  const swalButton = document.getElementById('water-button');
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options).then(callback); // <-- add the `.then(callback)`
+    });
+  }
+};
+
+export { initSweetalertWater };
